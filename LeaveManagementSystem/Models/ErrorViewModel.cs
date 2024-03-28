330e67b6d460cf8c337b0306e79fca36ab1213b1
shaik <shaik@LAPTOP-PJE7CUIF>
2024-03-28 23:40:40 +0530

    Add project files.

---
+++ LeaveManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
